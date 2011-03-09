892fa107a6b753aa60f503e5fc9a5bfc831a2802
Lele Gaifax <lele@metapensiero.it>
2011-03-09 13:52:41 +0000

    Added my own specific preferences as an example, now that they are password-clean

---
+++ lele.el
@@ -0,0 +1,34 @@
+;-*- coding: utf-8 -*-
+;:Progetto:  dot.emacs -- Lele's personal preferences
+;:Creato:    ven 09 apr 2010 01:08:16 CEST
+;:Autore:    Lele Gaifax <lele@metapensiero.it>
+;:Licenza:   GNU General Public License version 3 or later
+;
+
+;; Claws-mail
+
+(setq auto-mode-alist
+      (cons '("/tmpmsg\\." . text-mode)
+            auto-mode-alist))
+(setq magic-mode-alist nil)
+
+
+;; ERC
+
+(setq erc-autojoin-channels-alist
+      '(("freenode.net"
+         "#rafanass" "#linuxtrent" "#darcs" "#revctrl"
+         "#tailor" "#sqlalchemy" "#airpim")))
+(setq erc-email-userid "lele@metapensiero.it")
+(setq erc-nick "lelit")
+
+
+;; GNUS
+
+(setq gnus-home-directory (concat user-specific-dir "/gnus/"))
+(setq gnus-read-newsrc-file nil)
+(setq gnus-save-newsrc-file nil)
+
+(setq gnus-default-posting-charset (quote utf-8))
+(setq gnus-permanently-visible-groups ".")
+(setq gnus-summary-line-format "%U%R%z%d %(%[%-20,20a%]%) %I%s\n")

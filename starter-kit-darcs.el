e055a3e2617b57e5fc53e8b4430b7c845d2bb7f6
Lele Gaifax <lele@metapensiero.it>
2011-04-15 19:25:04 +0000

    Autoload darcsum-repository-root, used by whatsnew-or-vc-dir

--- starter-kit-darcs.el
+++ starter-kit-darcs.el
@@ -3,8 +3,9 @@
 
 ;; darcsum -- http://joyful.com/repos/darcsum
 (autoload 'darcsum-changes "darcsum" nil t)
-(autoload 'darcsum-whatsnew "darcsum" nil t)
+(autoload 'darcsum-repository-root "darcsum" nil t)
 (autoload 'darcsum-view "darcsum" nil t)
+(autoload 'darcsum-whatsnew "darcsum" nil t)
 
 ;; vc-darcs
 (add-to-list 'vc-handled-backends 'DARCS)

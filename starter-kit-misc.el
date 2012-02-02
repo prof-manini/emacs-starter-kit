ec1f584ca749fe177e3bbdbf6b99ccd98ac11f28
Lele Gaifax <lele@metapensiero.it>
2012-02-02 05:15:10 +0000

    Fix whitespace-indent-tabs-mode default value to match indent-tabs-mode

--- starter-kit-misc.el
+++ starter-kit-misc.el
@@ -106,11 +106,13 @@
 
 ;; by default do not use TABs
 (set-default 'indent-tabs-mode nil)
+(set-default 'whitespace-indent-tabs-mode nil)
 
 ;; but Makefiles are an exception, TAB is mandatory at bol
 (add-hook 'makefile-mode-hook
           #'(lambda ()
               (setq indent-tabs-mode t)
+              (setq whitespace-indent-tabs-mode t)
               (add-to-list (make-local-variable 'whitespace-style) 'indentation::tab)
               (add-hook 'before-save-hook 'whitespace-cleanup)
               ))

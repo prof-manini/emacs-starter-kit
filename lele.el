3288f44e14575e402752f32395ec2f73adc41392
Lele Gaifax <lele@metapensiero.it>
2013-01-16 19:04:15 +0000

    Do not hide joins and departures from irc channels

--- lele.el
+++ lele.el
@@ -66,7 +66,7 @@
 
 (setq erc-track-exclude-types '("JOIN" "NICK" "PART" "QUIT" "MODE"
                                 "324" "329" "332" "333" "353" "477"))
-(setq erc-hide-list '("JOIN" "PART" "QUIT" "NICK"))
+;; (setq erc-hide-list '("JOIN" "PART" "QUIT" "NICK"))
 
 
 ;; GNUS

162a6335a09447f180a105553a3658b3dc25a6af
Lele Gaifax <lele@metapensiero.it>
2013-07-23 07:29:25 +0000

    Remove last vestiges of egg, and drop mo-git-blame
    The latter is eventually available thru marmelade

--- starter-kit-git.el
+++ starter-kit-git.el
@@ -1,16 +1,6 @@
 ;;; starter-kit-git.el --- Some git helpers
 ;;
 
-;; egg -- https://github.com/byplayer/egg
-(require 'egg)
-
-;; Switch to the status buffer, instead of splitting current one
-(setq egg-switch-to-buffer t)
-
-;; Map nicer git-specific alternative of vc-annotate on S-F9
-(autoload 'mo-git-blame-current "mo-git-blame" nil t)
-(define-key egg-minor-mode-map [S-f9] 'mo-git-blame-current)
-
 ;; (eval-after-load 'magit
 ;;   '(progn
 ;;      (set-face-foreground 'magit-diff-add "green3")

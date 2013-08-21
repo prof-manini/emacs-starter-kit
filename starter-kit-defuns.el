6a59a2b6f2ae2c077b5af63612caa4cee2dcccb2
Lele Gaifax <lele@metapensiero.it>
2013-08-21 15:49:57 +0000

    Set the server-name using the MD5 of the virtual environment
    This allows multiple emacs clients, and magit will invoke the emacsclient
    with the "-s" option to use the current emacs.

--- starter-kit-defuns.el
+++ starter-kit-defuns.el
@@ -310,7 +310,9 @@ Symbols matching the text at point are put first in the completion list."
   (setq desktop-dirname (getenv "VIRTUAL_ENV"))
   (setq desktop-save t)
   (setq desktop-save-mode t)
-  (desktop-read desktop-dirname))
+  (desktop-read desktop-dirname)
+  (setq server-name (md5 desktop-dirname))
+  (server-start))
 
 (defun sort-words (reverse beg end)
   "Sort words in region alphabetically, in REVERSE if negative.

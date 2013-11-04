80c22ad3576140ced875cd02433566c8212abf7a
Lele Gaifax <lele@metapensiero.it>
2013-11-04 05:12:06 +0000

    Full screen magit-status

--- starter-kit-git.el
+++ starter-kit-git.el
@@ -1,10 +1,21 @@
 ;;; starter-kit-git.el --- Some git helpers
 ;;
 
-;; (eval-after-load 'magit
-;;   '(progn
-;;      (set-face-foreground 'magit-diff-add "green3")
-;;      (set-face-foreground 'magit-diff-del "red3")))
+(eval-after-load 'magit
+  '(progn
+     ;; (set-face-foreground 'magit-diff-add "green3")
+     ;; (set-face-foreground 'magit-diff-del "red3")))
+
+     ;; full screen magit-status
+
+     (defadvice magit-status (around magit-fullscreen activate)
+       (window-configuration-to-register :magit-fullscreen)
+       ad-do-it
+       (delete-other-windows))
+
+     (defadvice magit-mode-quit-window (around magit-restore-screen activate)
+       ad-do-it
+       (jump-to-register :magit-fullscreen))))
 
 (provide 'starter-kit-git)
 ;;; starter-kit-git.el ends here

aed47f671993fcd318d9d243e2f53951b43b32b6
Lele Gaifax <lele@metapensiero.it>
2014-03-25 08:17:08 +0000

    Remove yasnippet special case for python completion
    The code didn't work on recent Emacs, and apparently it isn't needed anymore.

--- esk/completion.el
+++ esk/completion.el
@@ -18,32 +18,3 @@
 (define-key ac-complete-mode-map "\r" 'ac-complete)
 (define-key ac-complete-mode-map "\M-n" 'ac-next)
 (define-key ac-complete-mode-map "\M-p" 'ac-previous)
-
-;; Disabling Yasnippet completion
-
-(eval-when-compile (require 'yasnippet))
-
-(defun epy-snips-from-table (table)
-  (with-no-warnings
-    (let ((hashtab (ac-yasnippet-table-hash table))
-          (parent (ac-yasnippet-table-parent table))
-          candidates)
-      (maphash (lambda (key value)
-                 (push key candidates))
-               hashtab)
-      (identity candidates)
-      )))
-
-(defun epy-get-all-snips ()
-  (let (candidates)
-    (maphash
-     (lambda (kk vv) (push (epy-snips-from-table vv) candidates)) yas--tables)
-    (apply 'append candidates))
-  )
-
-(setq ac-ignores (concatenate 'list ac-ignores (epy-get-all-snips)))
-
-(defun ac-python-mode-setup ()
-  (add-to-list 'ac-sources 'ac-source-yasnippet))
-
-(add-hook 'python-mode-hook 'ac-python-mode-setup)

d4ed2ddcd74f55f1c7a3275d0f76eba1e070c4d4
Lele Gaifax <lele@metapensiero.it>
2014-10-15 18:26:42 +0000

    With a prefix argument, search the whole repository

--- esk/git.el
+++ esk/git.el
@@ -18,13 +18,17 @@
        (jump-to-register :magit-fullscreen))))
 
 (defun git-grep (command-args)
-  "Use the `grep' machinery to run `git grep'."
+  "Use the `grep' machinery to run `git grep'.
+Without a prefix argument the search is executed in the default-directory of the
+current buffer, otherwise it considers the whole Git repository."
   (interactive
    (let ((sap (thing-at-point 'symbol))
          (grep-command "git grep -n --color=always "))
      (list (read-shell-command "Run git grep (like this): "
                                (if sap (concat grep-command sap)
                                  grep-command)))))
+  (when current-prefix-arg
+      (setq command-args (concat "cd " (magit-get-top-dir) " && " command-args)))
   ; pipe thru cat, to avoid the "terminal not fully functional" error
   (compilation-start (concat command-args " | cat") 'grep-mode))
 

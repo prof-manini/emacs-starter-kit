661f8de5db328ee5d1d22c5e8e7e177177d3cd06
Lele Gaifax <lele@metapensiero.it>
2014-11-21 08:32:29 +0000

    Remove the colon from the electric-indent-chars in python-mode

--- esk/python.el
+++ esk/python.el
@@ -9,3 +9,9 @@
 (require 'flymake-cursor)
 
 (add-hook 'python-mode-hook 'flymake-python-pyflakes-load)
+
+;; Remove the colon from the electric-indent-chars, it almost always computes the wrong
+;; indentation (cfr https://emacs.stackexchange.com/questions/3322/python-auto-indent-problem)
+(add-hook 'python-mode-hook
+          (lambda ()
+            (setq electric-indent-chars (delq ?: electric-indent-chars))))

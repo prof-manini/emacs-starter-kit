661f9ef48e98660fba4741049f40842c36c29078
Lele Gaifax <lele@metapensiero.it>
2014-12-09 09:25:35 +0000

    Bind some python-nav-* functions in the python-mode-map

--- esk/python.el
+++ esk/python.el
@@ -15,3 +15,8 @@
 (add-hook 'python-mode-hook
           (lambda ()
             (setq electric-indent-chars (delq ?: electric-indent-chars))))
+
+(define-key python-mode-map (kbd "C-c b") 'python-nav-backward-defun)
+(define-key python-mode-map (kbd "C-c f") 'python-nav-forward-defun)
+(define-key python-mode-map (kbd "C-c u") 'python-nav-backward-statement)
+(define-key python-mode-map (kbd "C-c d") 'python-nav-forward-statement)

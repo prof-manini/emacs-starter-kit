2a1cf39aa2193267586f61e5b87efd0b716da55f
Lele Gaifax <lele@metapensiero.it>
2014-12-12 06:57:53 +0000

    Disable horizontal scrollbar, introduced in Emacs 25

--- init.el
+++ init.el
@@ -14,6 +14,7 @@
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(if (fboundp 'horizontal-scroll-bar-mode) (horizontal-scroll-bar-mode -1))
 
 ;; by default do not use TABs
 (set-default 'indent-tabs-mode nil)

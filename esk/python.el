2c32511e4d5659288beb32cc88e5e9aa681c44f3
Lele Gaifax <lele@metapensiero.it>
2014-12-29 12:08:49 +0000

    Remove the workaround to the Python auto indent problem
    Recent python-mode fixed the behaviour, so the colon effectively reindent
    only statements that usually require a dedent.

--- esk/python.el
+++ esk/python.el
@@ -10,12 +10,6 @@
 
 (add-hook 'python-mode-hook #'flymake-python-pyflakes-load)
 
-;; Remove the colon from the electric-indent-chars, it almost always computes the wrong
-;; indentation (cfr https://emacs.stackexchange.com/questions/3322/python-auto-indent-problem)
-(add-hook 'python-mode-hook
-          (lambda ()
-            (setq electric-indent-chars (delq ?: electric-indent-chars))))
-
 (define-key python-mode-map (kbd "C-c b") #'python-nav-backward-defun)
 (define-key python-mode-map (kbd "C-c f") #'python-nav-forward-defun)
 (define-key python-mode-map (kbd "C-c u") #'python-nav-backward-statement)

edca788ce68b12aa70a631ebecd6200ab8d30f52
Lele Gaifax <lele@metapensiero.it>
2014-12-29 13:41:03 +0000

    Postpone Python configurations after python.el load

--- esk/python.el
+++ esk/python.el
@@ -1,16 +1,16 @@
 ;;; esk/python.el --- setup of python stuff
 ;;
 
-(require 'python)
+(eval-after-load 'python
+  '(progn
+     (add-hook 'python-mode-hook #'esk/run-coding-hook)
 
-(add-hook 'python-mode-hook #'esk/run-coding-hook)
+     (require 'flymake-python-pyflakes)
+     (require 'flymake-cursor)
 
-(require 'flymake-python-pyflakes)
-(require 'flymake-cursor)
+     (add-hook 'python-mode-hook #'flymake-python-pyflakes-load)
 
-(add-hook 'python-mode-hook #'flymake-python-pyflakes-load)
-
-(define-key python-mode-map (kbd "C-c b") #'python-nav-backward-defun)
-(define-key python-mode-map (kbd "C-c f") #'python-nav-forward-defun)
-(define-key python-mode-map (kbd "C-c u") #'python-nav-backward-statement)
-(define-key python-mode-map (kbd "C-c d") #'python-nav-forward-statement)
+     (define-key python-mode-map (kbd "C-c b") #'python-nav-backward-defun)
+     (define-key python-mode-map (kbd "C-c f") #'python-nav-forward-defun)
+     (define-key python-mode-map (kbd "C-c u") #'python-nav-backward-statement)
+     (define-key python-mode-map (kbd "C-c d") #'python-nav-forward-statement)))

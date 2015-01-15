fddce3b76fc0f049d99bd68e7a3232451e51a4d5
Lele Gaifax <lele@metapensiero.it>
2015-01-15 19:25:16 +0000

    Do not add the marmalade ELPA archive

--- init.el
+++ init.el
@@ -51,7 +51,7 @@
 (require 'package)
 
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
+;(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
 
 (package-initialize)
 

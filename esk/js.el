2dca7b23f06d1308f111796a3de2e3757f2eb6c2
Lele Gaifax <lele@metapensiero.it>
2015-01-15 19:21:34 +0000

    Prefix function names with esk

--- esk/js.el
+++ esk/js.el
@@ -7,23 +7,23 @@
   (require 'js2-mode)
   (require 'hl-line))
 
-(defun js2-apply-jsl-declares ()
+(defun esk/js2-apply-jsl-declares ()
   "Extract top level //jsl:declare XXX comments"
   (setq js2-additional-externs
-        (nconc (js2-get-jsl-declares)
+        (nconc (esk/js2-get-jsl-declares)
                js2-additional-externs)))
 
-(defun js2-get-jsl-declares ()
+(defun esk/js2-get-jsl-declares ()
   (loop for node in (js2-ast-root-comments js2-mode-ast)
         when (and (js2-comment-node-p node)
                   (save-excursion
                     (goto-char (+ 2 (js2-node-abs-pos node)))
                     (looking-at "jsl:declare ")))
-        append (js2-get-jsl-declares-in
+        append (esk/js2-get-jsl-declares-in
                 (match-end 0)
                 (js2-node-abs-end node))))
 
-(defun js2-get-jsl-declares-in (beg end)
+(defun esk/js2-get-jsl-declares-in (beg end)
   (let (res)
     (save-excursion
       (goto-char beg)
@@ -45,7 +45,8 @@
      ;; Register some mode-specific hooks
      (add-hook 'js2-mode-hook
                (lambda ()
-                 (add-hook 'js2-post-parse-callbacks #'js2-apply-jsl-declares nil 'local)
+                 ;; Parse additional externs
+                 (add-hook 'js2-post-parse-callbacks #'esk/js2-apply-jsl-declares nil 'local)
                  (js2-reparse t)))
 
      ;; Activate imenu support

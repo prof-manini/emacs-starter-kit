4d68b787244bfe902077d65f0dd9c58762f58e90
Lele Gaifax <lele@metapensiero.it>
2015-08-23 18:49:56 +0000

    With two prefix arguments the esk/git-grep starts at the outmost upper repo

--- esk/git.el
+++ esk/git.el
@@ -16,31 +16,52 @@
 
 (require 'magit)
 
-(defun esk/git-grep (command-args)
+(defun esk/git-outer-level (&optional dir)
+  "Return the outer repository toplevel directory, or nil if there isn't one."
+  (unless dir
+    (setq dir (magit-toplevel)))
+  (magit-toplevel (file-name-directory (directory-file-name dir))))
+
+(defun esk/git-toplevel (&optional outmost)
+  "Return the toplevel directory of current repository.
+When OUTMOST is non-nil, assume this is a nested repository (a
+submodule for example) and return the directory containing the
+outmost repository instead."
+  (let ((toplevel (magit-toplevel))
+        outer)
+    (when outmost
+      (while (setq outer (esk/git-outer-level toplevel))
+        (setq toplevel outer)))
+    toplevel))
+
+(defun esk/git-grep (command)
   "Use the `grep' machinery to run `git grep'.
-Without a prefix argument the search is executed in the current Git repository,
-otherwise it recurses down also in all submodules."
+Without a universal prefix argument, the search is executed only
+in the current Git repository, otherwise it recurses down also in
+all submodules.
+With two univeral prefix arguments, the search starts in the
+outmost Git repository recursing down in all submodules."
   (interactive
    (let ((what (or (thing-at-point 'symbol t)
                    (read-from-minibuffer "Regexp to search: ")))
+         (toplevel (esk/git-toplevel (equal current-prefix-arg '(16))))
          (git-grep "git --no-pager grep -n --color=always ")
-         (git-submodule-foreach "git --no-pager submodule --quiet foreach --recursive ")
-         (sed (concat "sed \"s,^,$toplevel/$path/,;s,^" default-directory ",,\""))
          grep-command)
      (if current-prefix-arg
-         (setq grep-command (concat "(" git-grep "\"" what "\" || :) && "
-                                    git-submodule-foreach
-                                    "'(" git-grep "\"" what "\" | " sed ") || :'"))
+         (let ((git-submodule-foreach "git --no-pager submodule --quiet foreach --recursive ")
+               (sed (concat "sed \"s,^,$toplevel/$path/,;s,^"
+                            (expand-file-name toplevel) ",,\"")))
+           (setq grep-command (concat "(" git-grep "\"" what "\" || :) && "
+                                      git-submodule-foreach
+                                      "'(" git-grep "\"" what "\" | " sed ") || :'")))
        (setq grep-command (concat git-grep "\"" what "\"")))
-     (list (read-shell-command "Run: " grep-command))))
-  (compilation-start (concat "cd " (magit-toplevel) " && " command-args) 'grep-mode))
-
+     (list (read-shell-command "Run: " (concat "cd " toplevel " && " grep-command)))))
+  (compilation-start command 'grep-mode))
 
 (defun esk/parent-magit-status ()
   "Run magit-status on the parent repository"
   (interactive)
-  (let ((parent-dir (or (locate-dominating-file
-                         (file-name-directory (directory-file-name (magit-toplevel))) ".git") ".")))
-    (if (equal parent-dir ".")
+  (let ((parent-dir (esk/git-outer-level)))
+    (if (not parent-dir)
         (beep)
       (magit-status parent-dir))))

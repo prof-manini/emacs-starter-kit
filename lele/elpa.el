e9e754831d4f23f941d04cfea77dd1257a9e50c6
Lele Gaifax <lele@metapensiero.it>
2015-10-08 10:50:08 +0000

    Explicitly list the ELPA packages I effectively use

--- lele/elpa.el
+++ lele/elpa.el
@@ -1,3 +1,10 @@
 ;;; lele/elpa.el --- Lele's ELPA specialization
 
+(push 'dockerfile-mode esk/packages)
+(push 'jabber esk/packages)
+(push 'nginx-mode esk/packages)
 (push 'notmuch esk/packages)
+(push 'notmuch-labeler esk/packages)
+(push 'rainbow-mode esk/packages)
+(push 's esk/packages)
+(push 'sphinx-doc esk/packages)

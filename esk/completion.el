1eff00a5ff77049334b0e274da3b0b8de96ee56d
Lele Gaifax <lele@metapensiero.it>
2015-12-05 18:44:24 +0000

    Sort company candidates honoring the case of current prefix

--- esk/completion.el
+++ esk/completion.el
@@ -11,4 +11,20 @@
 (setq company-dabbrev-downcase nil)
 (setq company-dabbrev-ignore-case t)
 
+(defun esk/company-sort-preferring-exact-or-same-case (candidates)
+  (let ((exact nil)
+        (sameprefix nil)
+        (others nil)
+        (plen (length company-prefix)))
+    (mapc (lambda (candidate)
+            (if (equal company-prefix candidate)
+                (push candidate exact)
+              (if (equal company-prefix (substring candidate 0 plen))
+                  (push candidate sameprefix)
+                (push candidate others))))
+          candidates)
+    (append exact sameprefix others)))
+
+(add-to-list 'company-transformers #'esk/company-sort-preferring-exact-or-same-case)
+
 (global-company-mode)

bb67904ff52f6bd27d90dbad5b9372e2058303eb
Lele Gaifax <lele@metapensiero.it>
2015-12-05 21:57:26 +0000

    Bind visual-line-mode toggler to C-f12

--- esk/bindings.el
+++ esk/bindings.el
@@ -103,6 +103,7 @@
 (global-set-key [f11] #'esk/activate-virtual-desktop)
 
 (global-set-key [f12] #'auto-fill-mode)
+(global-set-key [C-f12] #'visual-line-mode)
 (global-set-key [S-f12] #'linum-mode)
 (global-set-key [M-f12] #'menu-bar-mode)
 

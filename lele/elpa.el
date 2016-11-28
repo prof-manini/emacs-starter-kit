c67b575db65e9e113f93245612f12df55ad3dfb1
Lele Gaifax <lele@metapensiero.it>
2016-11-28 15:27:27 +0100

    Remove packages I do not use anymore

--- lele/elpa.el
+++ lele/elpa.el
@@ -2,10 +2,8 @@
 
 (push 'dockerfile-mode esk/packages)
 (push 'erc-hl-nicks esk/packages)
-(push 'jabber esk/packages)
 (push 'nginx-mode esk/packages)
 (push 'notmuch esk/packages)
 (push 'notmuch-labeler esk/packages)
 (push 'rainbow-mode esk/packages)
 (push 's esk/packages)
-(push 'sphinx-doc esk/packages)
